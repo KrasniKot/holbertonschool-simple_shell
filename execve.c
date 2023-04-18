6288b67aab6a776d7b2e09b791a25d73e32ace91
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-04-18 09:41:23 -0700

    execve, fork, wait

--- execve.c
+++ execve.c
@@ -1,2 +1,33 @@
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
 
+void main(void)
+{
+	char command[] = "ls -l /tmp";
+	char *ps = strtok(command, " ");
+	int i = 0;
+	char *pps[4];
+	pid_t pid_child;
+
+	for (; ps; i++)
+	{
+		pps[i] = ps;
+		ps = strtok(NULL, " ");
+	}
+	pps[i] = NULL;
+
+	pid_child = fork();
+	if (pid_child == 0)
+	{
+		if (execve("/usr/bin/ls", pps, NULL) == -1)
+			perror("/usr/bin/ls");
+	}
+	else if (pid_child > 0)
+	{
+		wait(&pid_child);
+	}
+	else
+		perror("fork failed");
+}
