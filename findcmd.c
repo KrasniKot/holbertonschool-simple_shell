cc7e3efa68f4c58c2f2ccff9399b3a6c3b4c04b9
KrasniKot <6187@holbertonstudents.com>
2023-04-25 10:11:16 -0700

    where allocated

--- findcmd.c
+++ findcmd.c
@@ -14,7 +14,7 @@ char *findcmd(char *cmd, char *path)
 	char **tpath = tokenizer(path, ":");
 	char *in;
 	char *u_cmd;
-	char where[60];
+	char *where;
 
 	u_cmd = malloc(sizeof(char) * (k + 2));
 	u_cmd[0] = 47;
@@ -23,14 +23,16 @@ char *findcmd(char *cmd, char *path)
 	u_cmd[k + 1] = '\0';
 	for (i = 0; tpath[i]; i++)
 	{
+		where = malloc(sizeof(char) * (k + 2 + strlen(tpath[i])));
 		strcpy(where, tpath[i]);
 		strcat(where, u_cmd);
 		if (!stat(where, &st))
 		{
 			in = strcat(tpath[i], u_cmd);
-			free(tpath), free(u_cmd);
+			free(tpath), free(u_cmd), free(where);
 			return (in);
 		}
+		free(where);
 	}
 	in = strcat(tpath[0], u_cmd);
 	free(tpath), free(u_cmd);
